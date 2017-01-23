93354617f4d196af72166247ece74d7fb796b00a
root <root@robotTech>
2017-01-23 19:02:59 +0800

    Data show

--- sample/dataProcess/dataProcess.py
+++ sample/dataProcess/dataProcess.py
@@ -1,1 +1,23 @@
+import pymongo
+import pprint
+
+#print all the data in a mongo collection
+def datashow(col):
+    for c in col.find():
+        pprint.pprint(c)
+
+def main():
+    client = pymongo.MongoClient()
+    db = client.xdrs
+    col = db.logs
+    datashow(col)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
 
