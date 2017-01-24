5085d40c2a9db3fe5975d5d19ce41e647dc99a97
root <root@robotTech>
2017-01-24 18:24:10 +0800

    pick up chat logs done

--- sample/dataProcess/dataProcess.py
+++ sample/dataProcess/dataProcess.py
@@ -1,16 +1,29 @@
 import pymongo
 import pprint
+import json
+import logging
+import bson
 
 #print all the data in a mongo collection
-def datashow(col):
+def dataShow(col):
     for c in col.find():
         pprint.pprint(c)
+
+def dataFile(col):
+    with open('chatLogs', 'w') as f:
+        for c in col.find():
+            out = c['snick'].encode('utf-8').strip() + " "
+            out += c['cnick'].encode('utf-8').strip() + " "
+            out += c['msg'].encode('utf-8').strip()
+            out += "\n"
+            f.write(out)
+    logging.warning('Have write chat logs into logs file')
 
 def main():
     client = pymongo.MongoClient()
     db = client.xdrs
     col = db.logs
-    datashow(col)
+    dataFile(col)
 
 if __name__ == "__main__":
     main()
